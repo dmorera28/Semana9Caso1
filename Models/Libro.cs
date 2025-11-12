8bf4993f182f32dbcc000508331eac89934d3568
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-11 22:15:38 -0600

    Validaciones con Annotations en el modelo

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -1,22 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
 namespace Semana9Caso1.Models
 {
     public class Libro
     {
-        public string Titulo { get; set; }
+        [Required(ErrorMessage = "El título del libro es obligatorio")]
+        [MinLength(3, ErrorMessage = "El título del libro debe tener como mínimo 3 caracteres")]
+        public string Titulo { get; set; } = string.Empty;
 
-        public string Autor { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "El autor del libro debe tener como mínimo 3 caracteres")]
+        public string Autor { get; set; } = string.Empty;
 
-        public string Categoria { get; set; }
+        [Required]
+        
+        public string Categoria { get; set; } = string.Empty;
 
-        public DateTime AñoPublic { get; set; }
+        [Required]
+        public DateTime AñoPublic { get; set; } = DateTime.Now;
 
+        [Required]
+        
         public int NumPag { get; set; }
 
-        public string Cod { get; set; }
+        [Required]
+        public string Cod { get; set; } = string.Empty;
 
-        public bool Disponible { get; set; }
+        [Required]
+        public bool Disponible { get; set; } = true;
     }
 }
