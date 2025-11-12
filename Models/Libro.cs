3ff6677a0749f238062da83431fcbe8c56520b95
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-12 01:05:38 -0600

    Actualización del modelo con validaciones

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -8,25 +8,25 @@ namespace Semana9Caso1.Models
         [MinLength(3, ErrorMessage = "El título del libro debe tener como mínimo 3 caracteres")]
         public string Titulo { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar el nombre del autor del libro")]
         [MinLength(3, ErrorMessage = "El autor del libro debe tener como mínimo 3 caracteres")]
         public string Autor { get; set; } = string.Empty;
 
         [Required]
-        
         public string Categoria { get; set; } = string.Empty;
 
-        [Required]
-        public DateTime AñoPublic { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Debe ingresar el año de publicación del libro")]
+        [DataType(DataType.Date)]
+        public DateTime AñoPublic { get; set; } = DateTime.Today;
 
-        [Required]
-        
+        [Required(ErrorMessage = "Debe ingresar el número de páginas del libro")]
         public int NumPag { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar el código del libro")]
+        [RegularExpression(@"^(LIB)-\d+$", ErrorMessage = "El código del libro debe comenzar con ´LIB-´ en mayúsculas antes de ingresar los números correspondientes")]
         public string Cod { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Debe indicar la disponibilidad del libro")]
         public bool Disponible { get; set; } = true;
     }
 }
