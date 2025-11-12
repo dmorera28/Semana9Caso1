232d4260a4b166e2ca48f1007ad840c3a36c83fb
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-11 20:59:18 -0600

    Nombres de atributos establecidos

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -6,5 +6,17 @@ namespace Semana9Caso1.Models
     public class Libro
     {
         public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Categoria { get; set; }
+
+        public DateTime AñoPublic { get; set; }
+
+        public int NumPag { get; set; }
+
+        public string Cod { get; set; }
+
+        public bool Disponible { get; set; }
     }
 }
